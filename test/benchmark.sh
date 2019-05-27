1454bec133f8bc51eca0e3d8b6d006576f8aed2b
Nadeesha Cabral <n@nadeesha.me>
2019-05-28 09:07:05 +1000

    Adding benchmark script
    Signed-off-by: Nadeesha Cabral <n@nadeesha.me>

--- test/benchmark.sh
+++ test/benchmark.sh
@@ -1,8 +1,35 @@
+#!/bin/bash
+
+echo "1. Creating npm link to the current working tree"
+npm link
+
+cd "$(dirname "$0")"
+
+echo "2. Initializing package.json and tsconfig.json"
 yarn init -y
 cp ../tsconfig.json ./tsconfig.json
-mkdir src
 
-for n in {1..999}
+echo "3. Linking ts-prune from step 1"
+npm link ts-prune
+
+echo "4. Generating files in the src..."
+rm -rf src
+mkdir src
+cp 0.template src/0.ts
+
+for n in {1..499}
 do
-    cat testFilContent.template | sed -En "s/__n__/$(expr $n - 1)/g" > "src/$n.ts"
+  let REFER_TO=n-1
+  cat testFileContent.template | sed "s/__n__/$REFER_TO/g" > "src/$n.ts"
 done
+
+echo "5. Run ts-prune"
+TIME_START=`date +%s`
+ts-prune
+TIME_END=`date +%s`
+COMPLETED_ON=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
+HOSTNAME=`hostname`
+
+echo "$HOSTNAME $COMPLETED_ON $((TIME_END-TIME_START))" >> ./benchmark_history.txt
+
+tail -1 ./benchmark_history.txt

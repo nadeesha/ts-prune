651488c221736dea3816a8ba9fcca3354ba24d2d
phiresky <phireskyde+git@gmail.com>
2020-06-24 15:52:17 +0200

    save after every symbol

--- src/fixer.ts
+++ src/fixer.ts
@@ -5,45 +5,47 @@ import { Node, SourceFile } from "ts-morph";
 
 export const fix = (state: State): void => {
   state.definitelyUnused().forEach((val) => {
-    let sf: SourceFile;
     val.symbols.forEach(({ symbol, name }) => {
+      let sf: SourceFile;
       const decls = symbol.getDeclarations();
       if (decls.length < 1) return;
-      let decl = decls[0];
-      sf = decl.getSourceFile();
-      if (Node.isVariableDeclaration(decl)) {
-        // e.g. `export const x = "foo";
-        // we need to get the outer node which is a VariableStatement which is an ExportableNode
-        decl = decl.getVariableStatement();
-      }
-      if (Node.isExportableNode(decl)) {
-        decl.setIsExported(false);
-        console.log("fixed ", val.file, name);
-      } else if (Node.isExportSpecifier(decl)) {
-        // e.g. export { default as Xyz } from "./src/...";'
-        if (decl.getExportDeclaration().getNamedExports().length === 1) {
-          // this is the only named export, remove the whole thing
-          decl.getExportDeclaration().remove();
-        } else {
-          // remove only this named export from the statement
-          decl.remove();
+      for (let decl of decls) {
+        if (sf && sf.getFilePath() !== decl.getSourceFile().getFilePath())
+          throw Error(`inconsistency`);
+        sf = decl.getSourceFile();
+        if (Node.isVariableDeclaration(decl)) {
+          // e.g. `export const x = "foo";
+          // we need to get the outer node which is a VariableStatement which is an ExportableNode
+          decl = decl.getVariableStatement();
         }
-      } else if (Node.isExportAssignment(decl)) {
-        // export xyz = foo;
-        decl.remove();
-      } else {
-        console.warn(
-          "could not fix ",
-          val.file,
-          name,
-          "unknown node type",
-          decl.getKindName(),
-          decl.getText()
-        );
+        if (Node.isExportableNode(decl)) {
+          decl.setIsExported(false);
+          console.log("fixed ", val.file, name);
+        } else if (Node.isExportSpecifier(decl)) {
+          // e.g. export { default as Xyz } from "./src/...";'
+          if (decl.getExportDeclaration().getNamedExports().length === 1) {
+            // this is the only named export, remove the whole thing
+            decl.getExportDeclaration().remove();
+          } else {
+            // remove only this named export from the statement
+            decl.remove();
+          }
+        } else if (Node.isExportAssignment(decl)) {
+          // export xyz = foo;
+          decl.remove();
+        } else {
+          console.warn(
+            "could not fix ",
+            val.file,
+            name,
+            "unknown node type",
+            decl.getKindName(),
+            decl.getText()
+          );
+        }
       }
-    });
-    if (sf) {
+      // for some reason need to save between symbols not just afterwards
       sf.saveSync();
-    }
+    });
   });
 };

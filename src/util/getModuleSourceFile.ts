4f9c1180625876c50ff40d183a189fb95168255b
phiresky <phireskyde+git@gmail.com>
2020-06-24 12:06:22 +0200

    fix false positive on symlinked files, update dependencies

--- src/util/getModuleSourceFile.ts
+++ src/util/getModuleSourceFile.ts
@@ -1,4 +1,8 @@
 import { ImportDeclaration, ExportDeclaration } from "ts-morph";
+import { realpathSync } from "fs";
 
-export const getModuleSourceFile = (decl: ImportDeclaration | ExportDeclaration) =>
-  decl.getModuleSpecifierSourceFile()?.getFilePath() ?? null;
+export const getModuleSourceFile = (decl: ImportDeclaration | ExportDeclaration) => {
+  const fpath = decl.getModuleSpecifierSourceFile()?.getFilePath();
+  if(fpath) return realpathSync(fpath);
+  return null;
+}

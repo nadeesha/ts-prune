a31b8039d6b1d2df15054d4577fa9d9e33576b21
Vitaly Iegorov <vitalyiegorov@gmail.com>
2020-08-24 09:36:36 +0300

    feat(configuration): added cosmiconfig for file config feat(configuration): added commander for cli config fix(core): removed minimist fix(core): added eslint-config-prettier

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,32 @@
 #!/usr/bin/env node
+import { cosmiconfigSync }  from "cosmiconfig";
+import program  from "commander";
 
 import { run } from "./runner";
+<<<<<<< HEAD
+import { ConfigInterface } from "./config.interface";
+=======
+import {ConfigInterface} from "./config.interface";
+>>>>>>> 84907e2... feat(configuration): added cosmiconfig for file config
 
-run();
+const cliConfig = program
+    .option('-p, --project [project]', 'TS project configuration file (tsconfig.json)')
+    .parse(process.argv)
+
+const moduleName = 'ts-prune';
+const explorerSync = cosmiconfigSync(moduleName);
+const fileConfig = explorerSync.search();
+
+if (!fileConfig && !cliConfig) {
+    throw new Error(`Config not found.
+    You can read about the configuration options here:
+    https://github.com/nadeesha/ts-prune#configuration
+  `)
+}
+
+const config: ConfigInterface = {
+    project: 'tsconfig.json',
+    ...cliConfig.project ? cliConfig : fileConfig
+};
+
+run(config);

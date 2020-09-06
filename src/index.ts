347792cf3d3d0131667ca6b287effaada8992112
Vitalii Yehorov <vitalii@clearmove.com>
2020-09-06 11:20:52 +0300

    fix(configuration): fixed backward compatibility

--- src/index.ts
+++ src/index.ts
@@ -13,13 +13,6 @@ const moduleName = 'ts-prune';
 const explorerSync = cosmiconfigSync(moduleName);
 const fileConfig = explorerSync.search();
 
-if (!fileConfig && !cliConfig) {
-    throw new Error(`Config not found.
-    You can read about the configuration options here:
-    https://github.com/nadeesha/ts-prune#configuration
-  `)
-}
-
 const config: ConfigInterface = {
     project: 'tsconfig.json',
     ...cliConfig.project ? cliConfig : fileConfig

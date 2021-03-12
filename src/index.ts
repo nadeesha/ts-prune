da1d869db2fdced49ee5abe0d86f420a1efa7c42
William Candillon <wcandillon@gmail.com>
2021-03-12 13:19:47 +0100

    Export ResultSymbol

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 export { IConfigInterface } from "./configurator";
-export {run} from "./runner";
+export { run } from "./runner";
+export { ResultSymbol } from "./analyzer";
 
 import { getConfig } from "./configurator";
 import { run } from "./runner";

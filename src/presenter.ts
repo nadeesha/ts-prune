45659ce5e5d4613f3f5836d5b11a58c265c30e13
Mikhail Belyaev <michail.belyaevq@gmail.com>
2022-07-28 01:18:18 +0300

    fix: parse win separators (#182)

--- src/presenter.ts
+++ src/presenter.ts
@@ -11,10 +11,17 @@ const formatOutput = (file: string, result: ResultSymbol) => {
 export const present = (state: State): string[] => {
   const unused2D = state
     .definitelyUnused()
-    .map(result => ({
-      file: result.file.replace(process.cwd(), "").replace(new RegExp("^/"), ""),
-      symbols: result.symbols
-    }))
+    .map(result => {
+      const file = result.file.replace(process.cwd(), "").replace(new RegExp("^/"), "")
+      const filePath = isWin
+        ? file.replaceAll(path.win32.sep, path.posix.sep)
+        : file
+
+      return ({
+        file: filePath,
+        symbols: result.symbols
+      })
+    })
     .map(
       ({file, symbols}) => symbols.map(sym => formatOutput(file, sym))
     );

f5ff21a8360cba7160fb7101df2d33059c8766f1
Wlad Paiva <desenvolvedormg@gmail.com>
2023-01-12 17:25:38 -0300

    feat: add custom path for the config file

--- src/configurator.ts
+++ src/configurator.ts
@@ -1,12 +1,14 @@
 import { cosmiconfigSync } from "cosmiconfig";
 import program from "commander";
 import pick from "lodash/fp/pick";
+import chalk from "chalk";
 
 export interface IConfigInterface {
   project?: string;
   ignore?: string;
   error?: string;
   skip?: string;
+  config?: string;
 }
 
 const defaultConfig: IConfigInterface = {
@@ -14,33 +16,56 @@ const defaultConfig: IConfigInterface = {
   ignore: undefined,
   error: undefined,
   skip: undefined,
-}
+  config: undefined,
+};
 
 const onlyKnownConfigOptions = pick(Object.keys(defaultConfig));
 
-
 export const getConfig = () => {
-  const cliConfig = onlyKnownConfigOptions(program
-    .allowUnknownOption() // required for tests passing in unknown options (ex: https://github.com/nadeesha/ts-prune/runs/1125728070)
-    .option('-p, --project [project]', 'TS project configuration file (tsconfig.json)', 'tsconfig.json')
-    .option('-i, --ignore [regexp]', 'Path ignore RegExp pattern')
-    .option('-e, --error', 'Return error code if unused exports are found')
-    .option('-s, --skip [regexp]', 'skip these files when determining whether code is used')
-    .parse(process.argv))
+  const cliConfig = onlyKnownConfigOptions(
+    program
+      .allowUnknownOption() // required for tests passing in unknown options (ex: https://github.com/nadeesha/ts-prune/runs/1125728070)
+      .option(
+        "-p, --project [project]",
+        "TS project configuration file (tsconfig.json)",
+        "tsconfig.json"
+      )
+      .option("-i, --ignore [regexp]", "Path ignore RegExp pattern")
+      .option("-e, --error", "Return error code if unused exports are found")
+      .option(
+        "-s, --skip [regexp]",
+        "skip these files when determining whether code is used"
+      )
+      .option(
+        "-c, --config [path]",
+        "Path to config file (default: .ts-prunerc.json)"
+      )
+      .parse(process.argv)
+  );
 
   const defaultConfig = {
-    project: "tsconfig.json"
+    project: "tsconfig.json",
+  };
+
+  const moduleName = "ts-prune";
+  const explorerSync = cosmiconfigSync(moduleName);
+  let fileConfig;
+  try {
+    fileConfig = cliConfig.config
+      ? explorerSync.load(cliConfig.config).config
+      : explorerSync.search()?.config;
+  } catch (error) {
+    console.log(chalk.red(`Error loading config file\n${error.message}`));
+    process.exit(1);
   }
 
-  const moduleName = 'ts-prune';
-  const explorerSync = cosmiconfigSync(moduleName);
-  const fileConfig = explorerSync.search()?.config;
+  delete cliConfig.config;
 
   const config: IConfigInterface = {
     ...defaultConfig,
     ...fileConfig,
-    ...cliConfig
+    ...cliConfig,
   };
 
   return config;
-}
+};
